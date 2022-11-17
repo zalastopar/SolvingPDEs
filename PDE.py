23910e39e454b23c4bb5391603f663ee044938b8
zalastopar <zala.stopar1@gmail.com>
2022-11-17 16:13:54 +0100

    some new plots

--- PDE.py
+++ PDE.py
@@ -3,6 +3,10 @@
 import numpy as np
 # math functions
 import math 
+import cmath
+# plot function
+import matplotlib.pyplot as plt
+import cplot
 
 from numba import jit 
 
@@ -16,7 +20,7 @@ import matplotlib.cm as cmap
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(linewidth=np.inf)
 
-import matplotlib.pyplot as plt # side-stepping mpl backend
+ # side-stepping mpl backend
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -34,39 +38,17 @@ y_steps = int(math.floor(100 + hy)/hy)
 x_steps = int(math.floor(Nx + hx)/hx)
 
 
-'''def TDMASolve(a, b, c, d):
-    # https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm
-    n = len(a)
-    ac, bc, cc, dc = map(np.array, (a, b, c, d))
-    xc = []
-    for j in range(1, n):
-        if(bc[j - 1] == 0):
-            ier = 1
-            return
-        ac[j] = ac[j]/bc[j-1]
-        bc[j] = bc[j] - ac[j]*cc[j-1]
-    if(b[n-1] == 0):
-        ier = 1
-        return
-    for j in range(1, n):
-        dc[j] = dc[j] - ac[j]*dc[j-1]
-    dc[n-1] = dc[n-1]/bc[n-1]
-    for j in range(n-2, -1, -1):
-        dc[j] = (dc[j] - cc[j]*dc[j+1])/bc[j]
-    return dc
-'''
-
 # prepare Q and P
-Q=np.zeros((y_steps,y_steps))
-P=np.zeros((y_steps,y_steps))
+Q=np.zeros((y_steps,y_steps), dtype=np.complex_)
+P=np.zeros((y_steps,y_steps), dtype=np.complex_)
 
 for i in range (0,y_steps):
-    D = float(-1/(2*(i+1)))/k*hx/hy/hy
-    Q[i,i]=2+2*D
-    P[i,i]=2-2*D
+    D = (-1/(2*(1j+1)))/k*hx/hy/hy
+    Q[i,i]= 2 + 2*D
+    P[i,i]= 2 - 2*D
 
 for i in range (0,y_steps-1):  
-    D = float(-1/(2*(i+1)))/k*hx/hy/hy    
+    D = (-1/(2*(1j+1)))/k*hx/hy/hy    
     Q[i+1,i]=-D
     Q[i,i+1]=-D
     P[i+1,i]=D
@@ -74,23 +56,47 @@ for i in range (0,y_steps-1):
 
 
 # initial condition a0
-a0 = np.ones((y_steps, 1))
+a0 = np.zeros((y_steps, 1), dtype=np.complex_)
 for i in range(0, y_steps):
     y = y_points[i]
-    a0[i, 0] = math.exp(-k*k*float(y^2))
+    a0[y, 0] = math.exp(-k*k*(y*y))
 
 
-A = P.dot(a0) # calculated a_n
+s = [row[0] for row in a0]
+
+
+a1 = []
+for i in range(0, y_steps):
+    y = y_points[i]
+    a1.append(math.exp(-k*k*(y*y)))
+
+
+
+'''x = a1
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+plt.show()'''
+
+
 
 # a, b, c, d for TDMA
-D = np.zeros(y_steps)
+D = np.zeros(y_steps, dtype=np.complex_)
 
 for i in range (0,y_steps):
-    D[i] = float(-1/(2*(i+1)*hy*hy))/k*hx  
+    D[i] = (-1/(2*(1j+1)*hy*hy))/k*hx  
 
-a = np.ones(y_steps-1)*(-D[1:])
-c = np.ones(y_steps-1)*(-D[:-1])
-b = np.ones(y_steps)*2*(1+D)
+a = np.ones(y_steps-1, dtype=np.complex_)*(-D[1:])
+c = np.ones(y_steps-1, dtype=np.complex_)*(-D[:-1])
+b = np.ones(y_steps, dtype=np.complex_)*2*(1+D)
+
+A = np.zeros((y_steps, 1), dtype=np.complex_) # calculated a_n
+for i in range(len(a0)):
+    A[i, 0] = a0[i]
+
 
 
 R = P.dot(A)
@@ -98,16 +104,83 @@ R = P.dot(A)
 for x in range(1, x_steps+1):
     d = P.dot(A[:,x-1])
     x = np.linalg.solve(Q, d)
-
     s = x.reshape(y_steps, 1)
     A = np.hstack((A, s))
 
 np.set_printoptions(precision=3)
-print(A)
 
-xvals = x_points[3:5]
+
+#print(A[:, 0])
+
+x = a1
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+plt.title("A0 drawn real data type vector")
+plt.show()
+fig.savefig('lineA0-.png')
+
+# make data
+x = A[:, 0]
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+plt.title("A0 drawn with i type data (0*i)")
+plt.show()
+fig.savefig('lineA0.png')
+
+# make data
+x = A[:, 1]
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth=2.0)
+plt.title("A1")
+plt.show()
+fig.savefig('lineA1.png')
+
+# make data
+x = A[:, 2]
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth=2.0)
+plt.title("A2")
+plt.show()
+fig.savefig('lineA2.png')
+
+# make data
+x = A[:, 3]
+y = y_points
+
+# plot
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth=2.0)
+plt.title("A1")
+plt.show()
+fig.savefig('lineA3.png')
+
+# make data
+x = A[:, 2]
+y = y_points
+
+'''# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+plt.show()
+
+xvals = x_points[0:3]
 yvals = y_points
-zvals = A[:, 3:5]
+zvals = A[:, 0:3]
 
 heatmap, ax = plt.subplots()
 
@@ -117,4 +190,22 @@ ax.set(xlabel='some x', ylabel='some y')
 cbar = heatmap.colorbar(im)
 cbar.ax.set_ylabel('stuff')
 plt.show()
-heatmap.savefig('heatmap3-5.png')
+heatmap.savefig('heatmap3p.png')'''
+
+def huevalueplot(cmplxarray):
+    # Creating the black cover layer
+
+    black = np.full((*cmplxarray.shape, 4), 0.)
+    black[:,:,-1] = np.abs(cmplxarray) / np.abs(cmplxarray).max()
+    black[:,:,-1] = 1 - black[:,:,-1]
+
+    # Actual plot
+
+    fig, ax = plt.subplots()
+    # Plotting phases using 'hsv' colormap (the 'hue' part)
+    ax.imshow(np.angle(cmplxarray), cmap='hsv')
+    # Plotting the modulus array as the 'value' part
+    ax.imshow(black)
+    ax.set_axis_off()
+    plt.show()
+
--- testing_tridiag_matrix_alorith.py
+++ testing_tridiag_matrix_alorith.py
@@ -1,68 +1,19 @@
-# LIBRARY
-# vector manipulation
+import matplotlib.patheffects as path_effects
+import matplotlib.pyplot as plt
 import numpy as np
-# math functions
-import math 
+import pandas as pd
+from matplotlib.colors import LinearSegmentedColormap
+from scipy.ndimage import gaussian_filter
 
+from mplsoccer import Pitch, VerticalPitch, FontManager, Sbopen
 
-'''def TDMASolve(a, b, c, d):
-    # https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm
-    n = len(a)
-    ac, bc, cc, dc = map(np.array, (a, b, c, d))
-    xc = []
-    for j in range(1, n):
-        if(bc[j - 1] == 0):
-            ier = 1
-            return
-        ac[j] = ac[j]/bc[j-1]
-        bc[j] = bc[j] - ac[j]*cc[j-1]
-    if(b[n-1] == 0):
-        ier = 1
-        return
-    for j in range(1, n):
-        dc[j] = dc[j] - ac[j]*dc[j-1]
-    dc[n-1] = dc[n-1]/bc[n-1]
-    for j in range(n-2, -1, -1):
-        dc[j] = (dc[j] - cc[j]*dc[j+1])/bc[j]
-    return dc
-'''
-import numpy as np
-from numba import jit    
+# get data
+parser = Sbopen()
+match_files = [19789, 19794, 19805]
+df = pd.concat([parser.event(file)[0] for file in match_files])  # 0 index is the event file
 
-@jit
-def TDMA(a,b,c,d):
-    n = len(d)
-    w= np.zeros(n-1,float)
-    g= np.zeros(n, float)
-    p = np.zeros(n,float)
-    
-    w[0] = c[0]/b[0]
-    g[0] = d[0]/b[0]
-
-    for i in range(1,n-1):
-        w[i] = c[i]/(b[i] - a[i-1]*w[i-1])
-    for i in range(1,n):
-        g[i] = (d[i] - a[i-1]*g[i-1])/(b[i] - a[i-1]*w[i-1])
-    p[n-1] = g[n-1]
-    for i in range(n-1,0,-1):
-        p[i-1] = g[i-1] - w[i-1]*p[i]
-    return p
-
-
-
-
-a1 = [1, 0]
-b1 = [1, 1, 1]
-c1 = [2, 2]
-d1 = [2, 6, 3]
-
-r1 = TDMA(a1, b1, c1, d1)
-#print(r1)
-
-a2 = [5, 2]
-b2 = [2, 1, 4]
-c2 = [2, 3]
-d2 = [4, 17, 2]
-
-r2 = TDMA(a2, b2, c2, d2)
-#print(r2)
+# filter chelsea pressure and pass events
+mask_chelsea_pressure = (df.team_name == 'Chelsea FCW') & (df.type_name == 'Pressure')
+df_pressure = df.loc[mask_chelsea_pressure, ['x', 'y']]
+mask_chelsea_pressure = (df.team_name == 'Chelsea FCW') & (df.type_name == 'Pass')
+df_pass = df.loc[mask_chelsea_pressure, ['x', 'y', 'end_x', 'end_y']]
